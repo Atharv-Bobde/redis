eb7ef208decd3a062101456260fb3ab0b907833f
Atharv <atharvbobde123@gmail.com>
2025-05-01 19:31:12 +0530

    Refactor RESP handling: add utility functions and update CMake configuration

---
+++ src/include/RESP.utils.h
@@ -0,0 +1,10 @@
+//#pragma once is a non-standard but widely supported preprocessor directive that tells the compiler to include a header file only once per compilation unit, even if it’s included multiple times.
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void parseRESPArray(const string& input,vector<string> &result);
+void parseBulkStrings(const string& input,vector<string> &result) ;
+string toRESPBulkStrings(const vector<string>& items,int start,int end);
---
+++ src/utils/RESP.util.cpp
@@ -0,0 +1,93 @@
+#include "RESP.utils.h"
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+void parseRESPArray(const string& input,vector<string> &result) {
+
+    stringstream ss(input);
+    char type;
+    ss >> type;
+  
+    if (type != '*') {
+       cerr <<"Invalid RESP array format: expected '*', got '" + string(1, type) + "'";
+    }
+  
+   
+    int array_size;
+    ss >> array_size;
+    if (ss.fail() || array_size < 0) {
+        cerr <<"Invalid RESP array size";
+    }
+  
+    // Consume the newline character after reading the array size *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
+    ss.ignore(numeric_limits<streamsize>::max(), '\n');
+    char c =ss.peek();
+  
+    for (int64_t i = 0; i < array_size; ++i) {
+        if (ss.peek() != '$') {
+           cerr <<"Invalid RESP array element format: missing string indicator";
+        }
+        ss >> type; // Read '$'
+         if (type != '$') {
+           cerr <<"Invalid RESP array element format: expected '$', got '" + string(1, type) + "'";
+        }
+  
+        int64_t string_length;
+        ss >> string_length;
+         if (ss.fail() || string_length < 0) {
+           cerr <<"Invalid RESP string length";
+        }
+         // Consume the newline character after reading the string length
+        ss.ignore(numeric_limits<streamsize>::max(), '\n');
+  
+        string element;
+        element.resize(string_length);
+        ss.read(&element[0], string_length);
+  
+  
+        result.push_back(element);
+  
+        // Consume the newline character after reading the string data
+         ss.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+  
+  }
+  
+  string toRESPBulkStrings(const vector<string>& items,int start,int end) {
+    string resp;
+    for (int i = start; i < end; ++i) {
+      const string& item = items[i];
+        resp += "$" + to_string(item.size()) + "\r\n" + item + "\r\n";
+    }
+    return resp;
+  }
+  
+  void parseBulkStrings(const string& input,vector<string> &result) {
+    size_t pos = 0;
+  
+    while (pos < input.size()) {
+        if (input[pos] == '$') {
+            // Find end of length line
+            size_t len_end = input.find("\r\n", pos);
+            if (len_end == string::npos) break;
+  
+            // Parse length
+            int length = stoi(input.substr(pos + 1, len_end - pos - 1));
+            pos = len_end + 2;
+  
+            // Check bounds and extract the string
+            if (pos + length > input.size()) break;
+            string str = input.substr(pos, length);
+            result.push_back(str);
+  
+            pos += length + 2; // Move past content and \r\n
+        } else {
+            break;
+        }
+    }
+  }
